74192fc3458c12f450839adbcb62f7d34d8afe19
tag-770 <eeee.chanarivi@icloud.com>
2022-05-29 11:36:31 +0900

    修正しました。

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +17,6 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    $user = Auth::user();
-    dump($user);
     return view('welcome');
 })->name('toppage');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
