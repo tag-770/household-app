af428f5631bd28d304e18424b970db36af15222c
tag-770 <eeee.chanarivi@icloud.com>
2022-05-30 23:13:21 +0900

    use足しました。

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AccountController;
+
 
 /*
 |--------------------------------------------------------------------------
