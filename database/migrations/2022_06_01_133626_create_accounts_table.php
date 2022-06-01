ed4c8905a191a1c9e1b976043d3259d6bd782dec
tag-770 <eeee.chanarivi@icloud.com>
2022-06-01 23:14:35 +0900

    データベースを作成しました。

---
+++ database/migrations/2022_06_01_133626_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            //$table->id();
+            //$table->timestamps();
+            $table->unsignedBigInteger('id');
+            $table->string('title');
+            $table->integer('amount');
+            $table->text('memo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
