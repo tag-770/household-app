ffe02fee7e517f9d6af8ea7313bc6ce891bc62c1
tag-770 <eeee.chanarivi@icloud.com>
2022-06-01 23:38:21 +0900

    コメントアウトを解除しました。

--- database/migrations/2022_06_01_133626_create_accounts_table.php
+++ database/migrations/2022_06_01_133626_create_accounts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('accounts', function (Blueprint $table) {
-            //$table->id();
+            $table->id();
             //$table->timestamps();
             $table->unsignedBigInteger('id');
             $table->string('title');
