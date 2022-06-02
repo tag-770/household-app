7a8a334e1cf594f73cfab9d3bbfcb9aa8790fbd4
tag-770 <eeee.chanarivi@icloud.com>
2022-06-02 22:41:43 +0900

    timestampsを最後に移動しました。

--- database/migrations/2022_06_01_133626_create_accounts_table.php
+++ database/migrations/2022_06_01_133626_create_accounts_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('title');
             $table->integer('amount');
             $table->text('memo');
+            $table->timestamps();
         });
     }
 
