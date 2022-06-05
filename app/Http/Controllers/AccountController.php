1b662875f7ecfa6e783e425da7c1a8b531fb7de4
tag-770 <eeee.chanarivi@icloud.com>
2022-06-05 21:56:10 +0900

    データベースに格納しようとしましたが、できていません。

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Models\Account;
 
 class AccountController extends Controller
 {
@@ -13,6 +14,21 @@ class AccountController extends Controller
 
     public function accountsend()
     {
+        return redirect()->route('home');
+    }
+
+    public function create()
+    {
+        //インスタンス作成
+        $account = new Account();
+
+        //$account = Account::create();
+
+        Account::create([
+            'title' => $request->title,
+            'amount' => $request->amount,
+            'memo' => $request->memo,
+        ]);
         return redirect()->route('home');
     }
 }
