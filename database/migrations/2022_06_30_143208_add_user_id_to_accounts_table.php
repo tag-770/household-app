d45201871566a82837485f1c8d774007357d3526
tag-770 <eeee.chanarivi@icloud.com>
2022-07-03 15:35:53 +0900

    user-idカラムと外部キー制約を追加した

---
+++ database/migrations/2022_06_30_143208_add_user_id_to_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            // 外部キー制約の削除
+            $table->dropForeign('accounts_user_id_foreign');
+            // カラムの削除
+            $table->dropColumn('user_id');
+        });
+    }
+};
