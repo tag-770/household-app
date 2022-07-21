02841834a89e2bc7db5dcf419f9a66ee950cab24
tag-770 <eeee.chanarivi@icloud.com>
2022-07-21 23:10:25 +0900

    /accountsにアクセスすると「自分の家計簿一覧」が表示できるようにしました。

---
+++ resources/views/accounts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <header>
+    <!-- <nav class="my-navbar">
+      <a class="my-navbar-brand" href="/">ToDo App</a>
+    </nav> -->
+  </header>
+  <main>
+   <h1>自分の家計簿一覧</h1>
+  </main>
+</body>
+</html>
