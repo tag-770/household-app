1a3fc350cbff33551e3ba6afc6a417f51e337851
tag-770 <eeee.chanarivi@icloud.com>
2022-09-11 16:08:30 +0900

    headerを独立させた

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,7 @@
     <title>家計簿アプリ</title>
 </head>
 <body>
-    <header>
-        <h1><a href="{{ route('home') }}">家計簿アプリ</a></h1>
-        <a href="{{ route('logout') }}">ログアウト</a>
-        <a href="{{ route('accountcreate') }}">家計簿登録</a>
-        <a href="{{ route('accounts') }}">自分の家計簿</a>
-    </header>
+@include('layouts.header')
     <main>
         @yield('content')
     </main>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,6 @@
+<header class="header">
+    <h1><a href="{{ route('home') }}">家計簿アプリ</a></h1>
+    <a href="{{ route('logout') }}">ログアウト</a>
+    <a href="{{ route('accountcreate') }}">家計簿登録</a>
+    <a href="{{ route('accounts') }}">自分の家計簿</a>
+</header>
