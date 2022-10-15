becec4399bce3752f5228bf8b3132bf7eab6df61
tag-770 <eeee.chanarivi@icloud.com>
2022-10-15 20:46:04 +0900

    修正しました。

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -15,16 +15,16 @@
             <div class="w-full md:w-1/2 flex flex-col items-center " >
                 <h1 class="text-center text-2xl font-bold text-gray-600 mb-6">家計簿登録</h1>
                 <div class="w-3/4 mb-6">
-                <label for="title">名前</label>
-                    <input type="text" name="title" id="title" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500" placeholder="○○費">
+                    <label for="title">名前</label>
+                        <input type="text" name="title" id="title" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500" placeholder="○○費">
                 </div>
                 <div class="w-3/4 mb-6">
-                <label for="amount">金額</label>
-                    <input type="text" name="amount" id="amount" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 " placeholder="1000">
+                    <label for="amount">金額</label>
+                        <input type="text" name="amount" id="amount" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 " placeholder="1000">
                 </div>
                 <div class="w-3/4 mb-6">
-                <label for="memo">メモ</label>
-                    <input type="text" name="memo" id="memo" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 ">
+                    <label for="memo">メモ</label>
+                        <input type="text" name="memo" id="memo" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 ">
                 </div>
                 <!-- button -->
                 <div class="w-3/4 mt-4">
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -26,27 +26,27 @@
             <div class="flex justify-center h-screen w-screen items-center">
                 <div class="w-full md:w-1/2 flex flex-col items-center " >
                     <h1 class="text-center text-2xl font-bold text-gray-600 mb-6">新規ユーザー登録</h1>
-                        <div class="w-3/4 mb-6">
-                            <label for="username">ユーザー名</label>
+                    <div class="w-3/4 mb-6">
+                        <label for="username">ユーザー名</label>
                             <input type="text" name="username" id="username" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500">
-                        </div>
-                        <div class="w-3/4 mb-6">
-                          <label for="amount">メールアドレス</label>
+                    </div>
+                    <div class="w-3/4 mb-6">
+                        <label for="amount">メールアドレス</label>
                             <input type="email" name="email" id="email" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 ">
-                        </div>
-                        <div class="w-3/4 mb-6">
-                          <label for="memo">パスワード</label>
+                    </div>
+                    <div class="w-3/4 mb-6">
+                        <label for="memo">パスワード</label>
                             <input type="password" name="password" id="password" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 ">
-                        </div>
-                        <div class="w-3/4 mb-6">
-                          <label for="memo">確認用パスワード</label>
+                    </div>
+                    <div class="w-3/4 mb-6">
+                        <label for="memo">確認用パスワード</label>
                             <input type="password" name="password_confirmation" id="password_confirmation" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 ">
-                        </div>
-                        <!-- button -->
-                        <div class="w-3/4 mt-4">
-                          <button type="submit" class="py-4 bg-blue-400 w-full rounded text-blue-50 font-bold hover:opacity-75"> 送信</button>
-                        </div>
-                  </div>
+                    </div>
+                    <!-- button -->
+                    <div class="w-3/4 mt-4">
+                        <button type="submit" class="py-4 bg-blue-400 w-full rounded text-blue-50 font-bold hover:opacity-75"> 送信</button>
+                    </div>
+                </div>
             </div>
         </div>
     </form>
