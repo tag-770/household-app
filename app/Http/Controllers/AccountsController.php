2847585329ea129ed10b78d988a8afbcc7a09d1a
tag-770 <eeee.chanarivi@icloud.com>
2022-10-23 17:08:11 +0900

    修正しました。

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -8,15 +8,7 @@ class AccountsController extends Controller
 {
     public function show()
     {
-        return view("accounts/show");
-    }
-
-    public function accountsshow()
-    {
-        $my_accounts = Account::where('user_id', '=', 1)->get();
+        $my_accounts = Account::where('id', '=', 1)->first();
         return view("accounts/show")->with('accounts', $my_accounts);
-
     }
-
-    
 }
